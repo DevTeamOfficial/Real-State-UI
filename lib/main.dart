9013c885f17f89f24e12240021ab7b4681a4c5f0
ravndu-dlshan <159864490+ravndu-dlshan@users.noreply.github.com>
2025-04-15 16:51:36 +0530

    aythentication 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:real_estate_ui_tutorial/screens/onboarding.dart';
+import 'package:real_estate_ui_tutorial/screens/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         textTheme: GoogleFonts.latoTextTheme(),
         primaryColor: const Color(0xff35573B),
       ),
-      home: const OnboardingPage(),
+      home: const LoginPage(),
     );
   }
 }
